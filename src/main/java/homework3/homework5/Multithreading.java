6bdbbe725586dd6b0e4170bbfa362a5fd12758fb
sealchikk <sealchikk@gmail.com>
2022-05-20 00:32:29 +0900

    HW

---
+++ src/main/java/homework3/homework5/Multithreading.java
@@ -0,0 +1,60 @@
+package homework3.homework5;
+
+public class Multithreading {
+    static final int size = 10000000;
+    static final int h = size / 2;
+
+    public static void main(String[] args) {
+        float[] arr = new float[size];
+        m1(arr);
+        m2(arr);
+    }
+
+    public static void m1(float[] arr) {
+        for (int i = 0; i < size; i++) {
+            arr[i] = 1;
+        }
+        long a = System.currentTimeMillis();
+        for (int i = 0; i < size; i++) {
+            arr[i] = (float) (arr[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
+        }
+        System.out.println(System.currentTimeMillis() - a);
+    }
+
+    public static void m2(float[] arr) {
+        for (int i = 0; i < size; i++) {
+            arr[i] = 1;
+        }
+        float[] a1 = new float[h];
+        float[] a2 = new float[h];
+        long a = System.currentTimeMillis();
+        System.arraycopy(arr, 0, a1, 0, h);
+        System.arraycopy(arr, h, a2, 0, h);
+        Thread thread = new Thread(() -> {
+            for (int i = 0; i < h; i++) {
+                a1[i] = (float) (a1[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
+            }
+        });
+        thread.start();
+        Thread thread1 = new Thread(() -> {
+            for (int i = 0; i < h; i++) {
+                a1[i] = (float) (a1[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
+            }
+        });
+        thread1.start();
+        try {
+            thread.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        try {
+            thread1.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.arraycopy(a1, 0, arr, 0, h);
+        System.arraycopy(a2, 0, arr, h, h);
+        System.out.println(System.currentTimeMillis() - a);
+    }
+
+}
